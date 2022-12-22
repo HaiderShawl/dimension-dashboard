1d80b3b2f7b542a0ec9887742b52219d22275418
Haider Shawl <haidershawl@Haiders-MacBook-Air.local>
2022-12-22 19:58:18 +0530

    fonts

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2f8b2b6f4fdb6784"; }
+/******/ 		__webpack_require__.h = function() { return "c9c0a74227025582"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
